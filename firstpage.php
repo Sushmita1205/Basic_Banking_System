3726588bef545f24e8a0a2f421a0d3981d1c4b0c
Sushmita1205 <72659245+Sushmita1205@users.noreply.github.com>
2020-12-12 00:10:32 +0530

    Add files via upload

---
+++ firstpage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Banking System</title>
+    <link rel="stylesheet" href="firstpage.css">
+</head>
+<body>
+<img src="bank6.jpg" alt="Bank">
+<div>
+      <h1>Sparks Foundation Internship Project</h1>
+    <br><br>
+      <h2>Basic Banking System</h2>
+      <br>
+      <a href="index.php">
+      <button class="go" href="index.php">Go</button></a><br>
+</div>
+<br>
+<p><h1 style="text-align: right; color:white;">- by Sushmita Mondal<h1></p>
+</body>
+</html>
